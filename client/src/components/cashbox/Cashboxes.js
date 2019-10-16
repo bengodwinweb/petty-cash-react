d102a9516df9ee38aee9cead33b8d4aef4ee0903
Benjamin Godwin <ben.godwin1@gmail.com>
2019-10-15 22:04:16 -0400

    add button to cashboxes that allows access to new cashbox form

--- client/src/components/cashbox/Cashboxes.js
+++ client/src/components/cashbox/Cashboxes.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { MDBBtn, MDBIcon } from 'mdbreact';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import CashboxesList from './CashboxesList';
 
 class Cashboxes extends Component {
@@ -8,6 +10,23 @@ class Cashboxes extends Component {
       <div>
         <h2>Cashboxes</h2>
         <CashboxesList />
+        <Link to="/cashboxes/new">
+          <MDBBtn
+            href=""
+            color="blue"
+            className="p-0"
+            style={{
+              position: 'fixed',
+              bottom: '30px',
+              right: '35px',
+              width: '70px',
+              height: '70px',
+              borderRadius: '50%'
+            }}
+          >
+            <MDBIcon icon="plus" size="2x" />
+          </MDBBtn>
+        </Link>
       </div>
     );
   }
