8684bbe3a16bd42d4580b7fdb9c2e7f830bbac65
Benjamin Godwin <ben.godwin1@gmail.com>
2019-10-17 22:59:40 -0400

    header formatting to cashbox show route

--- client/src/components/cashbox/CashboxesShow.js
+++ client/src/components/cashbox/CashboxesShow.js
@@ -26,46 +26,55 @@ class CashboxShow extends Component {
     }
 
     return (
-      <MDBContainer className="mt-4 p-0">
-        <MDBRow className="">
-          <MDBCol className="">
-            <MDBJumbotron fluid className="m-0 py-4">
-              <MDBContainer className="px-5">
-                <h2 className="h1-responsive mt-4">{companyName}</h2>
-                <h2 className="h2-responsive mt-2 mb-4">{cashboxName}</h2>
-                <p className="my-2 lead" style={{ fontWeight: '200' }}>
-                  Fund Total: ${fundTotal}
-                </p>
-                <p className="lead" style={{ fontWeight: '200' }}>
-                  Remaining: ${currentBox.boxTotal}
-                </p>
-                <hr className="my-2" />
-                <div className="mt-4 mb-3 d-flex flex-wrap flex-md-nowrap justify-content-center justify-content-md-start">
-                  <Link to="/cashboxes">
+      <div>
+        <h2 className="ml-3 pt-2 display-4 pb-2">{companyName}</h2>
+        <MDBContainer className="mt-4 p-0">
+          <MDBRow className="">
+            <MDBCol className="">
+              <MDBJumbotron fluid className="m-0 py-4">
+                <MDBContainer className="px-5">
+                  <h2 className="h1-responsive mt-4 mb-3 pb-2">
+                    {cashboxName}
+                  </h2>
+                  <p className="my-2 lead" style={{ fontWeight: '200' }}>
+                    Fund Total: ${fundTotal}
+                  </p>
+                  <p className="lead" style={{ fontWeight: '200' }}>
+                    Remaining: ${currentBox.boxTotal}
+                  </p>
+                  <hr className="my-2" />
+                  <div className="mt-4 mb-3 d-flex flex-wrap flex-md-nowrap justify-content-center justify-content-md-start">
+                    <Link to="/cashboxes">
+                      <MDBBtn outline color="default" className="">
+                        Back
+                      </MDBBtn>
+                    </Link>
                     <MDBBtn outline color="default" className="">
-                      Back
+                      Edit
                     </MDBBtn>
-                  </Link>
-                  <MDBBtn outline color="default" className="">
-                    Edit
-                  </MDBBtn>
-                  <MDBBtn outline color="danger" size="small" className="small">
-                    Delete
-                  </MDBBtn>
-                </div>
-              </MDBContainer>
-            </MDBJumbotron>
-          </MDBCol>
-        </MDBRow>
+                    <MDBBtn
+                      outline
+                      color="danger"
+                      size="small"
+                      className="small"
+                    >
+                      Delete
+                    </MDBBtn>
+                  </div>
+                </MDBContainer>
+              </MDBJumbotron>
+            </MDBCol>
+          </MDBRow>
 
-        <div className="my-4">
-          <TransactionList />
-        </div>
+          <div className="my-4">
+            <TransactionList />
+          </div>
 
-        <div className="my-4">
-          <Box box={currentBox} />
-        </div>
-      </MDBContainer>
+          <div className="my-4">
+            <Box box={currentBox} />
+          </div>
+        </MDBContainer>
+      </div>
     );
   }
 }
